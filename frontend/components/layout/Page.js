a9324cbb263621fc1bc9d196caf33dc733c03a15
Guy Routledge <mail@guyroutledge.co.uk>
2019-10-24 17:48:07 +0100

    Add ThemeProvider and variables for theme styles

--- frontend/components/layout/Page.js
+++ frontend/components/layout/Page.js
@@ -1,28 +1,41 @@
 import Header from './Header'
 import Meta from './Meta'
-import styled from 'styled-components'
+import styled, { ThemeProvider, injectGlobal } from 'styled-components'
+
+const theme = {
+	red: 'red',
+	black: '#393939',
+	grey: '#3a3a3a',
+	lightGrey: '#e1e1e1',
+	offWhite: '#ededed',
+	maxWidth: '1000px',
+	shadow: '0 12px 24px 0 rgba( 0, 0, 0, 0.09 )',
+	spacing: '2rem'
+};
 
 const StyledPage = styled.div`
-	background:white;
-	color:black;
+	background: white;
+	color: ${ props => props.theme.black };
 `;
 const Wrapper = styled.div`
-	max-width:1200px;
-	margin:0 auto;
-	padding:2rem;
+	max-width: ${ props => props.theme.maxWidth };
+	margin: 0 auto;
+	padding: ${ props => props.theme.spacing };
 `;
 
 const Page = props => {
 	return (
-		<StyledPage>
-			<Meta />
-			<Header />
-			<main className="page">
-				<Wrapper>
-					{ props.children }
-				</Wrapper>
-			</main>
-		</StyledPage>
+		<ThemeProvider theme={ theme }>
+			<StyledPage>
+				<Meta />
+				<Header />
+				<main className="page">
+					<Wrapper>
+						{ props.children }
+					</Wrapper>
+				</main>
+			</StyledPage>
+		</ThemeProvider>
 	)
 }
 
